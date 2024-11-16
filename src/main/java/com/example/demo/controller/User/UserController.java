19df5aed15f3366f13cf965619ee03c5f01aee71
GustavoSiancas <u202020594@upc.edu.pe>
2024-11-16 13:31:24 -0500

    Update properties

--- src/main/java/com/example/demo/controller/User/UserController.java
+++ src/main/java/com/example/demo/controller/User/UserController.java
@@ -2,7 +2,6 @@ package com.example.demo.controller.User;
 
 import com.example.demo.controller.User.Request.LoginRequest;
 import com.example.demo.controller.User.Response.LoginResponse;
-import com.example.demo.entity.UserEntity;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
